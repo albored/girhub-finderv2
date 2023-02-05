97068ba4defe348d29929db628aad02f44933707
albored <alcayomedina@gmail.com>
2023-02-05 19:42:53 -0400

    refactorizando con useReducer

---
+++ src/context/github/GithubContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = import.meta.env.VITE_REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `${GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
