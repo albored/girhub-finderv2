93e485f416c80f1d78a110b64712f208ee1ea7dc
albored <alcayomedina@gmail.com>
2023-02-05 16:38:55 -0400

    primer commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  return (
+    <>
+      {/* Search components */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
